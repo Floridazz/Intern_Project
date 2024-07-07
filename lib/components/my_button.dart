3ff1f1a6046ccbf8b6c706ddca4a777b3d9b5e99
Floridazz <147526054+Floridazz@users.noreply.github.com>
2024-07-07 23:48:33 +0700

    first commit

---
+++ lib/components/my_button.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class MyButton extends StatefulWidget {
+  final Function()? onTap;
+
+  const MyButton({super.key, required this.onTap});
+
+  @override
+  _MyButtonState createState() => _MyButtonState();
+}
+
+class _MyButtonState extends State<MyButton> {
+  bool isPressed = false;
+
+  void _onTapDown(TapDownDetails details) {
+    setState(() {
+      isPressed = true;
+    });
+  }
+
+  void _onTapUp(TapUpDetails details) {
+    setState(() {
+      isPressed = false;
+    });
+  }
+
+  void _onTapCancel() {
+    setState(() {
+      isPressed = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: widget.onTap,
+      onTapDown: _onTapDown,
+      onTapUp: _onTapUp,
+      onTapCancel: _onTapCancel,
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 200),
+        padding: const EdgeInsets.all(25),
+        margin: const EdgeInsets.symmetric(horizontal: 25),
+        decoration: BoxDecoration(
+          color: isPressed ? Colors.grey[800] : Colors.black,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: const Center(
+          child: Text(
+            "Sign In",
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
